55919fc413695a476a4f8879688ecbe62ad6f50a
Valentin Colombat <valentincolombat@gmail.com>
2025-11-28 11:19:56 +0100

    feat: Add localStorage persistence and priority filter functionality - Implement localStorage to save and retrieve todos between sessions - Add filter system to display tasks by priority (All, Urgent, Medium, Low)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type Priority = "Urgente" | "Moyenne" | "Basse"
 
@@ -7,11 +7,19 @@ type Todo = {
   text: string;
   priority : Priority;
 }
+
 function App() {
 
   const [ input ,setInput ] = useState("")
   const [ priority , setPriority ] = useState<Priority>("Moyenne")
-  const [todos, setTodos ] = useState<Todo[]>([])
+  const savedTodos = localStorage.getItem("todos")
+  const initialTodos = savedTodos ? JSON.parse(savedTodos) : []
+  const [todos, setTodos ] = useState<Todo[]>(initialTodos)
+  const [filter, setFilter ] = useState<Priority | "Tous" >("Tous")
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  } , [todos])
   
   function addTodo(){
     if(input.trim() ===""){
@@ -31,6 +39,13 @@ function App() {
     console.log(newTodos);
   }
 
+   let filteredTodos :Todo [] = []
+
+   if(filter === "Tous") {
+    filteredTodos = todos
+   }else {
+    filteredTodos = todos.filter((todo) => todo.priority === filter)
+   }
 
   return (
     <div className ="flex justify-center">
@@ -56,6 +71,20 @@ function App() {
             Ajouter
           </button>
         </div>
+        <div className ="space-y-2 flex-1 h-fit">
+          <div className="flex flex-wrap gap-4">
+            <button 
+            className ={`btn btn-soft ${filter === "Tous" ? "btn-primary" : ""}`}
+            onClick={() => setFilter("Tous")}
+            >
+              Tous
+            </button>
+          </div>
+          {filteredTodos.length > 0 ? 
+        (<div> test </div>) : 
+        (<div>test2</div>)
+        }
+        </div> 
       </div>
     </div>
   )
